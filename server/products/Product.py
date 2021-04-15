4597a55c2f46dc38bec2ff7b1976da6a7c42ea04
Gerard Slowey <gerard.slowey2@mail.dcu.ie>
2021-04-15 23:31:59 +0100

    time for bed

--- server/products/Product.py
+++ server/products/Product.py
@@ -13,41 +13,51 @@ class Product:
         self.restock_quantity = restock_quantity
 
 
-def getproducts():
-    # read file and return as json
-    df_products = pd.read_csv("./products/products.csv")
-    return df_products.to_json(orient='records')
+def getproducts(a):
+    # read lock
+    b = a.gen_rlock()
+    if b.acquire(blocking=True, timeout=5):
+        try:
+            # read file and return as json
+            df_products = pd.read_csv("./products/products.csv")
+            return df_products.to_json(orient='records')
+        finally:
+            b.release()
 
 
-def getFutureAvailability(product, time):
-    try:
-        # set the cut off date for x months from now
-        cutOffDate = date.today() + relativedelta(months=+time)
+def getFutureAvailability(a, product, time):
+    b = a.gen_rlock()
+    if b.acquire(blocking=True, timeout=5):
+        try:
+            # set the cut off date for x months from now
+            cutOffDate = date.today() + relativedelta(months=+time)
 
-        # read the files
-        df_orders = pd.read_csv(orders_file)
-        df_products = pd.read_csv(products_file)
+            # read the files
+            df_orders = pd.read_csv(orders_file)
+            df_products = pd.read_csv(products_file)
 
-        # format the dates correctly for comparison
-        df_orders['date'] = pd.to_datetime(df_orders['date'], format="%d/%m/%Y").dt.date
-        # filter dates for orders within the specified time range
-        df_orders = df_orders.loc[df_orders['date'] <= cutOffDate]
+            # format the dates correctly for comparison
+            df_orders['date'] = pd.to_datetime(df_orders['date'], format="%d/%m/%Y").dt.date
+            # filter dates for orders within the specified time range
+            df_orders = df_orders.loc[df_orders['date'] <= cutOffDate]
 
-        # get the rows for the product in the product and orders file
-        df_products = df_products.loc[df_products['productName'] == product]
-        df_orders = df_orders.loc[df_orders['product'] == product]
+            # get the rows for the product in the product and orders file
+            df_products = df_products.loc[df_products['productName'] == product]
+            df_orders = df_orders.loc[df_orders['product'] == product]
 
-        # total orders for this product from all clients
-        totalProductOrders = df_orders['quantity'].sum()
+            # total orders for this product from all clients
+            totalProductOrders = df_orders['quantity'].sum()
 
-        # need to compare this with the stock and restocking rate
-        product_row_num = df_products[df_products['productName'] == product].index[0]
-        stock_level = df_products.loc[product_row_num]['stock_quantity']
+            # need to compare this with the stock and restocking rate
+            product_row_num = df_products[df_products['productName'] == product].index[0]
+            stock_level = df_products.loc[product_row_num]['stock_quantity']
 
-        productRestockQuantity = df_products.loc[product_row_num]['restock_quantity']
-        restockingForThisPeriod = productRestockQuantity * time
+            productRestockQuantity = df_products.loc[product_row_num]['restock_quantity']
+            restockingForThisPeriod = productRestockQuantity * time
 
-        projectedAvailability = (stock_level + restockingForThisPeriod) - totalProductOrders
-        return int(projectedAvailability)
-    except Exception as e:
-        return e
+            projectedAvailability = (stock_level + restockingForThisPeriod) - totalProductOrders
+            return int(projectedAvailability)
+        except Exception as e:
+            return e
+        finally:
+            b.release()
