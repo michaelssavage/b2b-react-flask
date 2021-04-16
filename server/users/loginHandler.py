9fa74cc1f3d3cbe9022a2e5d991c776f84f1e6bf
Gerard Slowey <gerard.slowey2@mail.dcu.ie>
2021-04-16 12:22:49 +0100

    reafactored signup, some bugs fixed

--- server/users/loginHandler.py
+++ server/users/loginHandler.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 
 class LoginHandler:
     def __init__(self, a):
@@ -7,35 +8,32 @@ class LoginHandler:
         self.writer_lock = a.gen_wlock()
 
     def signUp(self, name, password):
-        success = True
         fieldnames = ['name', 'password']
-
-        if self.writer_lock.acquire(blocking=True, timeout=5):
-            try:
-                with open(self.file,  mode='a+', encoding="utf-8", newline='') as csvfile:
-                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-                    if self.alreadyExists(name):
-                        print("That username already exists")
-                        success = False
-                    else:
+        if self.alreadyExists(name):
+            # need to return this instead
+            # print("That username already exists")
+            return False
+        else:
+            if self.writer_lock.acquire(blocking=True, timeout=5):
+                try:
+                    with open(self.file,  mode='a+', encoding="utf-8", newline='') as csvfile:
+                        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
                         writer.writerow({"name": name, "password": password})
-                        print("signup created")
-                return success
-            finally:
-                self.writer_lock.release()
+                        # print("signup created")
+                        return True
+                finally:
+                    self.writer_lock.release()
 
     def alreadyExists(self, name):
-        exists = False
         if self.reader_lock.acquire(blocking=True, timeout=5):
             try:
-                with open(self.file, mode='r', newline='') as csvfile:
-                    reader = csv.DictReader(csvfile)
-                    for row in reader:
-                        if row['userID'] == name:
-                            exists = True
-                            return exists
-                    return exists
+                df = pd.read_csv(self.file)
+                df = df.loc[df['userID'] == name]
+
+                if df.empty:
+                    return False
+                else:
+                    return True
             finally:
                 self.reader_lock.release()
 
